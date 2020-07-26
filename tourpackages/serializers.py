c7cef8c60835a32a2b0b609e2743e230b6ffe875
huxaiphaer <lutaayaidris@Lutaayas-MacBook-Pro.local>
2020-07-27 00:55:42 +0300

    (ft) Implement updating tour packages

--- tourpackages/serializers.py
+++ tourpackages/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
 from . import models
+from .models import Destinations, AvailableDates
 
 
 class DestinationSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+
     class Meta:
         fields = (
             'id',
@@ -18,6 +21,8 @@ class DestinationSerializer(serializers.ModelSerializer):
 
 
 class AvailableDatesSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+
     class Meta:
         fields = (
             'id',
@@ -61,7 +66,31 @@ class TourPackageSerializer(serializers.ModelSerializer):
             models.Destinations.objects.create(**dest_data, tour_package=tour_package)
 
         for dd in dates_data:
-            models.AvailableDates.objects.create(**dd, tour_package=tour_package,)
+            models.AvailableDates.objects.create(**dd, tour_package=tour_package, )
 
         return tour_package
 
+    def update(self, instance, validated_data):
+        instance.name = validated_data['name']
+        instance.description = validated_data['description']
+        instance.price = validated_data['price']
+        instance.capacity = validated_data['capacity']
+        instance.save()
+
+        # Update destinations
+        for des in validated_data['destinations']:
+            des_id = des.get('id', None)
+            if des_id:
+                destination = Destinations(id=des['id'], location=des['location'], tour_type=des['tour_type'],
+                                           danger_type=des['danger_type'], tour_package=instance)
+                destination.save()
+
+        # Updating available dates
+        for a_dates in validated_data['available_dates']:
+            a_dates_id = a_dates.get('id', None)
+            if a_dates_id:
+                available_dates = AvailableDates(id=a_dates['id'], date_available=a_dates['date_available'],
+                                                 tour_package=instance)
+                available_dates.save()
+
+        return instance
